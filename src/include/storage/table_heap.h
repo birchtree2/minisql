c4d213c196521abd7a019113e446e1504434cd2f
birchtree2 <1980878786@qq.com>
2024-06-10 12:15:55 +0800

    table heap没加构造函数

--- src/include/storage/table_heap.h
+++ src/include/storage/table_heap.h
@@ -114,6 +114,10 @@ class TableHeap {
         log_manager_(log_manager),
         lock_manager_(lock_manager) {
     // ASSERT(false, "Not implemented yet.");
+    //这里需要分配一个新的页
+    auto page=reinterpret_cast<TablePage*>(buffer_pool_manager->NewPage(first_page_id_));
+    page->Init(first_page_id_,INVALID_PAGE_ID,log_manager_,txn);
+    buffer_pool_manager_->UnpinPage(first_page_id_,false);
   };
 
   explicit TableHeap(BufferPoolManager *buffer_pool_manager, page_id_t first_page_id, Schema *schema,
